68b800b1bf8d65907f24a5acb07de7bc5c340e5c
chenkai036 <1811702+chenkai036@users.noreply.github.com>
2018-10-10 17:09:54 +0100

    fix formatting the month in slog file directory (#176)

--- bin/hog/path.C
+++ bin/hog/path.C
@@ -12,7 +12,7 @@ std::string instantiateDir(const std::string& groupName, const std::string& dir)
   time_t now = ::time(0);
   if (const tm* t = localtime(&now)) {
     std::ostringstream ss;
-    ss << t->tm_year + 1900 << "." << (t->tm_mon < 10 ? "0" : "") << t->tm_mon + 1 << "." << (t->tm_mday < 10 ? "0" : "") << t->tm_mday;
+    ss << t->tm_year + 1900 << "." << (t->tm_mon < 9 ? "0" : "") << t->tm_mon + 1 << "." << (t->tm_mday < 10 ? "0" : "") << t->tm_mday;
     x = hobbes::str::replace<char>(x, "$DATE", ss.str());
   }
 
