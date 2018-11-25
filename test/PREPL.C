681352a2400b83d2b7858e5a19887f7f29cee17e
Kalani Thielen <kthielen@users.noreply.github.com>
2018-11-25 00:00:45 -0500

    add some tests for machine-REPL interaction, fix an "illegal instruction" problem on macOS

---
+++ test/PREPL.C
@@ -0,0 +1,43 @@
+
+#include <hobbes/hobbes.H>
+#include <hobbes/ipc/prepl.H>
+#include "test.H"
+
+using namespace hobbes;
+static cc& c() { static cc x; return x; }
+
+static proc* hiSession() {
+  static proc p;
+  static bool init = false;
+  if (!init) {
+    spawn("./hi -z", &p);
+  }
+  return &p;
+}
+
+static std::string exprEval(const std::string& x) {
+  proc* p = hiSession();
+  procEval(p, x);
+  std::ostringstream ss;
+  procRead(p, &ss);
+  return ss.str();
+}
+
+std::string exprTypeof(const std::string& x) {
+  proc* p = hiSession();
+  procTypeof(p, x);
+  std::ostringstream ss;
+  procRead(p, &ss);
+  return ss.str();
+}
+
+TEST(PREPL, EvalBasicExprs) {
+  EXPECT_EQ(exprEval("1+1"), "2");
+  EXPECT_TRUE(exprEval("undefinedVariableNameForTest").find("undefinedVariableNameForTest") != std::string::npos);
+}
+
+TEST(PREPL, Typeof) {
+  EXPECT_EQ(exprTypeof("1+1"), "int");
+  EXPECT_EQ(exprTypeof("[x|x<-[0..10],x%2==0]"), "[int]");
+}
+
