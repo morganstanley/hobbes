b99abf3eb7fb062389481929331a23346dc8dd4c
Kalani Thielen <kalant@ms.com>
2019-01-28 15:21:44 -0500

    add sqrt and log to the init functions

--- lib/hobbes/eval/funcdefs.C
+++ lib/hobbes/eval/funcdefs.C
@@ -645,6 +645,8 @@ void initStdFuncDefs(cc& ctx) {
   ctx.bind("ceil",   &lceil);
   ctx.bind("floor",  &lfloor);
   ctx.bind("truncd", &truncd);
+  ctx.bind("sqrt",   &sqrt);
+  ctx.bind("log",    &log);
 
   // this should never be called, it's only here to do something in the event of variant tag match failure
   ctx.bind(".failvarmatch", &failvarmatch);
