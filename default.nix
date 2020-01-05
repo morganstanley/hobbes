afb0b7c6f8dbd19fb9e851e469a982bbc5532ef4
Thomaz Leite <thomaz@actor.com.br>
2020-01-05 00:37:36 -0300

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs-channels/archive/nixos-unstable.tar.gz) {} }:
+
+pkgs.stdenv.mkDerivation {
+  name = "hobbes";
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+    gcc5
+    llvm_6
+    ncurses
+    python27
+    readline
+    zlib
+  ];
+}
