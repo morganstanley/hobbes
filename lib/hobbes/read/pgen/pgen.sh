432d6a2e52fa023bf86345ca165b67ab44f2963e
kthielen <kthielen@gmail.com>
2020-01-27 16:45:37 -0500

    fix pgen script to place parser header in one place consistently (no red herrings) (#327)

--- lib/hobbes/read/pgen/pgen.sh
+++ lib/hobbes/read/pgen/pgen.sh
@@ -5,5 +5,8 @@ cd `dirname ${BASH_SOURCE[0]}`
 # generate the LALR(1) parser and token definitions
 ${BISON:-bison} -d -ohexpr.parse.C hexpr.y
 
+# obey internal convention for division of source and header files
+mv hexpr.parse.H ../../../../include/hobbes/read/pgen/
+
 # generate the lexer to tokenize string input
 ${LEX:-flex} -ohexpr.lex.C hexpr.l
