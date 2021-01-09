5f72604ab5b431d973a5b78c3f9e198264bd69d9
Providence Salumu <Providence.Salumu@smunix.com>
2021-01-09 10:21:29 -0500

    travis: simplify and make reproducible builds nix: new derivations (hobbes-llvm-6, hobbes-llvm-8, hobbes-llvm-9, hobbes-llmv-10)

--- default.nix
+++ default.nix
@@ -1,22 +1,35 @@
-{ pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs-channels/archive/cc1ae9f21b9e0ce998e706a3de1bad0b5259f22d.tar.gz) {} }:
-
-pkgs.stdenv.mkDerivation {
-  name = "hobbes";
-  src = ./.;
-
-  nativeBuildInputs = with pkgs; [
+{ pkgs ? import (fetchTarball { url="https://github.com/NixOs/nixpkgs/archive/a3ab47ec9067b5f9fccda506fc8641484c3d8e73.tar.gz";
+                                sha256="1f1q89ka73ksvl5hgbrf624x0niwm2rg3dzxiscir0bji6zvjy15";
+                              }) {} }:
+with pkgs;
+let
+  commonBuildInputs = [
     ccls
     cmake
     ninja
     meson
-    gcc8
-    llvm_6
     ncurses
     python27
     readline
     zlib
   ];
-
+  src = ./.;
   doCheck = true;
   checkTarget = "test";
-}
+  llvmSupportVersion = [ 6 8 9 10 ];
+  defaultGCC = gcc10;
+  hobbesDrv = version: gccv: stdenv.mkDerivation {
+    pname = "hobbes-llvm";
+    version = builtins.toString version;
+    inherit src doCheck checkTarget;
+    nativeBuildInputs = commonBuildInputs ++ [ gccv (builtins.getAttr ("llvm_" + (builtins.toString version)) pkgs) ];
+  };
+in
+  builtins.listToAttrs (builtins.map (v: { name = "hobbes-llvm-" + (toString v); value = hobbesDrv v defaultGCC; }) llvmSupportVersion) // 
+  {
+    hobbes = stdenv.mkDerivation {
+      name = "hobbes";
+      inherit src doCheck checkTarget;
+      nativeBuildInputs = commonBuildInputs ++ [ gcc8 llvm_6 ];
+    };
+  }
