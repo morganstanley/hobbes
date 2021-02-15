9e2cb2136273ec36e6cb2f847335266b3161d863
Providence Salumu <Providence.Salumu@smunix.com>
2021-02-15 08:39:26 -0500

    ~/P/c/hobbes (fix.mcmove ± ) ∫ nix shell .#hobbesPackages/gcc-10/llvm-6/mcmove -c mcmove
    warning: Git tree '/home/smunix/Programming/compilers/hobbes' is dirty
    
    "(\\xs.match xs with | [1,2,3] -> 1 | [1,2,y] -> y | [] -> 9 | _ -> 10) :: [int] -> int"
    =====================================================================================
    00000000  48                            dec    %eax
    00000001  b8 b0 55 41 e3                mov    $0xe34155b0,%eax
    00000006  65 7f 00                      jg     0x9
    00000009  00 c3                         add    %al,%ebx
    "(\\xs.1) :: [int] -> int"
    =======================
    00000000  48                            dec    %eax
    00000001  b8 10 30 41 e3                mov    $0xe3413010,%eax
    00000006  65 7f 00                      jg     0x9
    00000009  00 c3                         add    %al,%ebx
    "1"
    =
    00000000  b8 01 00 00 00                mov    $0x1,%eax
    00000005  c3                            ret
    "2"
    =
    00000000  b8 02 00 00 00                mov    $0x2,%eax
    00000005  c3                            ret
    "\"providence\""
    ============
    00000000  48                            dec    %eax
    00000001  b8 00 d0 40 e3                mov    $0xe340d000,%eax
    00000006  65 7f 00                      jg     0x9
    00000009  00 c3                         add    %al,%ebx
    ~/P/c/hobbes (fix.mcmove ± )

--- playground/haskell/mcmove/src/Main.hs
+++ playground/haskell/mcmove/src/Main.hs
@@ -5,26 +5,24 @@ module Main where
 
 import System.IO
 import Control.Monad
+import Control.Arrow
 import Data.ByteString as B hiding (putStrLn, zip)
 import qualified Data.ByteString.Internal as BI
 import Data.Function
 import Foreign.C.Types
+import Data.Word
 import Foreign.Concurrent (newForeignPtr)
 import Foreign.ForeignPtr hiding (newForeignPtr)
 import Foreign.Ptr
 import qualified Language.C.Inline as C
 import qualified Language.C.Inline.Cpp as C
 import Lens.Micro.Platform
+import Text.Disassembler.X86Disassembler
 
 newtype Cc where
   Cc :: {_compiler :: ForeignPtr Cc} -> Cc
 
 makeLenses ''Cc
-
-newtype CharLen where
-  CharLen :: {_charLen :: ForeignPtr CharLen} -> CharLen
-
-makeLenses ''CharLen
 
 C.context $
   C.baseCtx
@@ -34,15 +32,13 @@ C.context $
     <> C.funCtx
     <> C.bsCtx
     <> C.cppTypePairs
-      [ ("Cc", [t|Cc|]),
-        ("CharLen", [t|CharLen|])
+      [ ("Cc", [t|Cc|])
       ]
 
 C.include "<hobbes/hobbes.H>"
 C.include "<iostream>"
 
 C.using "Cc = hobbes::cc"
-C.using "CharLen = std::pair<unsigned char*, size_t>"
 
 class New a where
   new :: IO a
@@ -54,54 +50,29 @@ instance New Cc where
         newForeignPtr cc ([C.block|void { delete $(Cc* cc); }|])
           >>= return . Cc
 
-class Convert b m a where
-  convert :: a -> m b
+withCC :: (Cc -> IO ()) -> IO ()
+withCC fn = new >>= fn
 
-instance Convert ByteString IO CharLen where
-  convert cl = do
-    c <- [C.exp|unsigned char*{ $fptr-ptr:(CharLen *cl')->first }|]
-    l <- [C.exp|size_t{ $fptr-ptr:(CharLen *cl')->second }|]
-    packCStringLen (castPtr c, fromIntegral l)
-    where
-      !cl' = cl ^. charLen
-
-disassemble :: Cc -> ByteString -> IO ByteString
-disassemble cc = getMachineCode >=> convert
+disass :: Cc -> (ByteString -> IO ()) -> ByteString -> IO ()
+disass cc fn bstr = do
+  [C.block|void {
+          auto bytes = $fptr-ptr:(Cc* cc')->machineCodeForExpr(std::string($bs-ptr:bstr, $bs-len:bstr));
+          $fun:(void(*bstrFn)(uint8_t const*, size_t))(bytes.data(), bytes.size());
+          }|]
   where
-    getMachineCode :: ByteString -> IO CharLen
-    getMachineCode bstr = do
-      cl <- [C.block|CharLen*{
-              auto bytes = $fptr-ptr:(Cc* cc')->machineCodeForExpr(std::string($bs-ptr:bstr, $bs-len:bstr));
-              auto r = new CharLen(new uint8_t[bytes.size()], bytes.size());
-              std::memcpy(r->first, bytes.data(), bytes.size());
-              std::cout << "disassemble:" << std::string(reinterpret_cast<char*>(bytes.data()), bytes.size()) << std::endl;
-              std::cout << "size:disassemble:" << bytes.size() << std::endl;
-              return r;
-              }|]
-      newForeignPtr cl ([C.block|void{ delete [] $(CharLen *cl)->first; delete $(CharLen *cl);}|])
-        >>= return . CharLen
-      where
-        !cc' = cc ^. compiler
+    bstrFn :: Ptr Word8 -> CSize -> IO ()
+    bstrFn ptr sz = packCStringLen (castPtr ptr, fromIntegral sz) >>= fn
+    cc' :: ForeignPtr Cc
+    !cc' = cc ^. compiler
 
 main :: IO ()
-main = do
-  (cc :: Cc) <- new
-  disassemble cc "(\\xs.match xs with | [1,2,3] -> 1 | [1,2,y] -> y | [] -> 9 | _ -> 10) :: [int] -> int"
-        >>= putStrLn . show . zip [1..] . fmap BI.w2c . B.unpack
-  x <-
-    [C.block| int{
-                 auto c = [&]()->hobbes::cc& {
-                   thread_local hobbes::cc cc;
-                   return cc;
-                 };
-                 auto disassemble = [&](std::string const& expr, unsigned char*& p, size_t& len) -> void {
-                   auto bytes = c().machineCodeForExpr(expr);
-                   p = bytes.data();
-                   len = bytes.size();
-                 };
-                 auto x = new int{};
-                 auto y = c().compileFn<int()>("{a=1,b=2,c=3}.b")();
-                 printf("Some number: %.2f\n", cos(0.5));
-                 return (uint64_t)y;
-               } |]
-  putStrLn $ show x ++ " characters printed."
+main = withCC \cc -> do
+  ["(\\xs.match xs with | [1,2,3] -> 1 | [1,2,y] -> y | [] -> 9 | _ -> 10) :: [int] -> int", "(\\xs.1) :: [int] -> int", "1", "2", "\"providence\""]
+    & traverseOf traverse (\x ->
+                             [putStrLn . show, putStrLn . uncurry Prelude.replicate . (B.length &&& const '='), disass cc printASM]
+                             <&> ($ x)
+                             & sequence)
+  return ()
+  where
+    printASM :: ByteString -> IO ()
+    printASM = (fmap (<&> (<&> showAtt)) . disassembleList . B.unpack) >=> (traverseOf (traversed .traversed) putStrLn) >=> (void . return)
