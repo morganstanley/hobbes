30574be2455852c3d9d98d1e554873e61481d7b7
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2021-08-04 18:46:00 +0000

    Make jit lazy (#416)
    * enable concurrent compilation inside LLJIT
    
    * add lazy jit
    
    * make function calls and declaration which return bool consistent with definitions (zeroext)
    
    * use angle brackets on include headers

--- lib/hobbes/eval/orcjitcc.C
+++ lib/hobbes/eval/orcjitcc.C
@@ -6,74 +6,20 @@
 #include <hobbes/hobbes.H>
 #include <hobbes/util/llvm.H>
 
+#include <llvm/ExecutionEngine/JITSymbol.h>
 #include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include <llvm/ExecutionEngine/Orc/Core.h>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/Support/AllocatorBase.h>
+#include <llvm/Support/Compiler.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/Process.h>
 
-namespace hobbes {
-
-ORCJIT::ORCJIT(std::unique_ptr<llvm::TargetMachine> tm,
-               const llvm::DataLayout &dl)
-    : targetMachine(std::move(tm)), dataLayout(dl),
-      mangle(execSession, dataLayout),
-      objectLayer(
-          execSession,
-          []() { return std::make_unique<llvm::SectionMemoryManager>(); }),
-      compileLayer(execSession, objectLayer,
-                   std::make_unique<llvm::orc::SimpleCompiler>(*targetMachine)),
-      optLayer(execSession, compileLayer, optimizeModule),
-      mainJD(execSession.createBareJITDylib(createUniqueName("main"))) {
-  mainJD.addGenerator(
-      cantFail(llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
-          dataLayout.getGlobalPrefix())));
-}
-
-#if LLVM_VERSION_MAJOR >= 12
-ORCJIT::~ORCJIT() {
-  // https://repo.hca.bsc.es/gitlab/rferrer/llvm-epi/-/commit/0aec49c8531bc5282b095730d34681455826bc2c
-  // newly added in llvm12, has to be called to destroy objectLayer associated memory manager
-  llvm::cantFail(execSession.endSession());
-}
-#else
-ORCJIT::~ORCJIT() = default;
-#endif
-
-llvm::Expected<std::unique_ptr<ORCJIT>> ORCJIT::create() {
-  auto jtmb = llvm::orc::JITTargetMachineBuilder::detectHost();
-  if (!jtmb) {
-    return jtmb.takeError();
-  }
-
-  auto tm = jtmb->createTargetMachine();
-  if (!tm) {
-    return tm.takeError();
-  }
-
-  auto dl = jtmb->getDefaultDataLayoutForTarget();
-  if (!dl) {
-    return dl.takeError();
-  }
-
-  return std::make_unique<ORCJIT>(std::move(*tm), *dl);
-}
-
-llvm::Error ORCJIT::addModule(std::unique_ptr<llvm::Module> m) {
-  return withContext([&](auto&) {
-    m->setDataLayout(dataLayout);
-    return optLayer.add(mainJD, llvm::orc::ThreadSafeModule(std::move(m), threadSafeContext()));
-  });
-}
-
-llvm::Expected<llvm::JITEvaluatedSymbol> ORCJIT::lookup(llvm::StringRef name) {
-  return execSession.lookup({&mainJD}, mangle(name.str()));
-}
-
-llvm::Error ORCJIT::addExternalSymbol(llvm::StringRef name, void *ptr) {
-  return mainJD.define(llvm::orc::absoluteSymbols(
-      {{mangle(name), llvm::JITEvaluatedSymbol::fromPointer(ptr)}}));
-}
-
+namespace {
 llvm::Expected<llvm::orc::ThreadSafeModule>
-ORCJIT::optimizeModule(llvm::orc::ThreadSafeModule tsm,
-                       const llvm::orc::MaterializationResponsibility &) {
+optimizeModule(llvm::orc::ThreadSafeModule tsm,
+               const llvm::orc::MaterializationResponsibility &) {
   tsm.withModuleDo([](llvm::Module &m) {
     auto fpm = llvm::legacy::FunctionPassManager(&m);
     fpm.add(llvm::createReassociatePass());
@@ -91,6 +37,69 @@ ORCJIT::optimizeModule(llvm::orc::ThreadSafeModule tsm,
   });
 
   return tsm;
+}
+} // namespace
+
+namespace hobbes {
+
+ORCJIT::ORCJIT() {
+  // this is for testing
+  // by default, no threading is enabled
+  const auto tn = [] {
+    const auto clamp = [](int v, int lo, int hi) -> int {
+      return v < lo ? lo : (hi < v ? hi : v);
+    };
+    if (const auto n = llvm::sys::Process::GetEnv("HOBBES_COMPILE_THREADS")) {
+      const int i = std::atoi(n->c_str());
+      return static_cast<unsigned int>(
+          clamp(i, 0, static_cast<int>(std::thread::hardware_concurrency())));
+    }
+    return 0U;
+  }();
+
+  llvm::orc::LLLazyJITBuilder jitBuilder;
+  jit = llvm::cantFail(
+      jitBuilder
+          .setJITTargetMachineBuilder(
+              llvm::cantFail(llvm::orc::JITTargetMachineBuilder::detectHost()))
+          .setNumCompileThreads(tn)
+          .setLazyCompileFailureAddr(llvm::pointerToJITTargetAddress(+[] {
+            throw std::runtime_error("exiting on lazy call through failure");
+          }))
+          .create());
+  jit->getIRTransformLayer().setTransform(optimizeModule);
+  jit->getMainJITDylib().addGenerator(
+      cantFail(llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
+          jit->getDataLayout().getGlobalPrefix())));
+
+  mangle = std::make_unique<llvm::orc::MangleAndInterner>(
+      jit->getExecutionSession(), jit->getDataLayout());
+}
+
+ORCJIT::~ORCJIT() = default;
+
+llvm::Error ORCJIT::addModule(std::unique_ptr<llvm::Module> m) {
+  return withContext([&, this](auto &) {
+    return jit->addLazyIRModule(
+        llvm::orc::ThreadSafeModule(std::move(m), threadSafeContext()));
+  });
+}
+
+llvm::Expected<llvm::JITEvaluatedSymbol> ORCJIT::lookup(llvm::StringRef name) {
+  return jit->lookup(name);
+}
+
+llvm::Error ORCJIT::addExternalCallableSymbol(llvm::StringRef name, void *ptr) {
+  return jit->getMainJITDylib().define(llvm::orc::absoluteSymbols(
+      {{(*mangle)(name), llvm::JITEvaluatedSymbol::fromPointer(
+                             ptr, llvm::JITSymbolFlags::Exported |
+                                      llvm::JITSymbolFlags::Callable)}}));
+}
+
+llvm::Error ORCJIT::addExternalNonCallableSymbol(llvm::StringRef name,
+                                                 void *ptr) {
+  return jit->getMainJITDylib().define(llvm::orc::absoluteSymbols(
+      {{(*mangle)(name), llvm::JITEvaluatedSymbol::fromPointer(ptr)}}));
 }
 } // namespace hobbes
 #endif
